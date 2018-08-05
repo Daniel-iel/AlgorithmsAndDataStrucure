f03b709bfdc2d678b693ae219a8d274443b8baaa
Daniel de Oliveira <iel_182@hotmail.com>
2018-08-05 14:07:57 -0300

    Resolving reverse string

--- StringReversalCode/Program.cs
+++ StringReversalCode/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace StringReversalCode
 {
@@ -6,7 +8,40 @@ namespace StringReversalCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var reversed1 = reverse1("Daniel");
+            Console.WriteLine(reversed1);
+
+            var reversed2 = reverse2("Daniel");
+            Console.WriteLine(reversed2);
+
+            var reversed3 = reverse3("Daniel");
+            Console.WriteLine(reversed3);
         }
+
+        static string reverse1(string str)
+        {
+            var arr = str.Reverse().ToArray();
+
+            return new string(arr);
+        }
+
+        static string reverse2(string str)
+        {
+            string reversed = "";
+            foreach (var value in str)
+                reversed = value + reversed;
+
+            return reversed;
+        }
+
+        static string reverse3(string str)
+        {
+            //string reversed = "";
+
+            //var arr = str.ToArray();
+            //var teste = arr.Aggregate((a , b) => b  );
+
+            return "";
+        }     
     }
 }
