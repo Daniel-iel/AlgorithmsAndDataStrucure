b220c17febe8dd3bf5b2baf2bb533c46e917d7f2
Daniel de Oliveira <iel_182@hotmail.com>
2018-09-02 15:30:15 -0300

    resolving anagram

--- AnagramCode/Program.cs
+++ AnagramCode/Program.cs
@@ -1,12 +1,118 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AnagramCode
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
+            Anagram1("!Daniel 12", "21 Daniel! ");
+
             Console.WriteLine("Hello World!");
+        }
+
+        public static bool Anagram1(string strA, string strB)
+        {
+            Regex reg = new Regex(@"\s+");
+
+            strA = reg.Replace(strA, "").ToLower();
+            strB = reg.Replace(strB, "").ToLower();
+
+            if (strA.Length != strB.Length)
+            {
+                return false;
+            }
+
+            var aCharMap = new Dictionary<char, int>();
+            var bCharMap = new Dictionary<char, int>();
+
+            foreach (var letter in strA)
+            {
+                if (aCharMap.ContainsKey(letter))
+                    aCharMap[letter]++;
+                else
+                    aCharMap.Add(letter, 1);
+            }
+
+            foreach (var letter in strB)
+            {
+                if (bCharMap.ContainsKey(letter))
+                    bCharMap[letter]++;
+                else
+                    bCharMap.Add(letter, 1);
+            }
+
+            foreach (var letter in strA)
+            {
+                if (!aCharMap.ContainsKey(letter) && !bCharMap.ContainsKey(letter))
+                    return false;
+                else
+                    if (aCharMap[letter] != bCharMap[letter])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool Anagram2(string strA, string strB)
+        {
+            var aCharMap = buildMap(strA);
+            var bCharMap = buildMap(strB);
+
+            if (!aCharMap.ContainsKey(bCharMap.Select(c => c.Key)))
+                return false;
+
+            foreach (var letter in aCharMap)
+            {
+                if (aCharMap[letter.Key] != bCharMap[letter.Key])
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static Dictionary<char, int> buildMap(string str)
+        {
+            var strMap = new Dictionary<char, int>();
+
+            Regex reg = new Regex(@"\s+");
+            str = reg.Replace(str, "").ToLower();
+
+            foreach (var letter in str)
+            {
+                if (strMap.ContainsKey(letter))
+                    strMap[letter]++;
+                else
+                    strMap.Add(letter, 1);
+            }
+
+            return strMap;
+        }
+
+        private static bool ContainsKey(this Dictionary<char, int> dic, IEnumerable<char> keys)
+        {
+            foreach (var key in keys)
+            {
+                if (!dic.ContainsKey(key))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool Anagram3(string strA, string strB)
+        {
+            return CleanStr(strA) == CleanStr(strB);
+        }
+
+        private static string CleanStr(string str)
+        {
+            return string.Concat(new Regex(@"\s+").Replace(str, "").ToLower().OrderBy(x => x));
         }
     }
 }
--- AnagramTest/AnagramTest.cs
+++ AnagramTest/AnagramTest.cs
@@ -1,3 +1,4 @@
+using AnagramCode;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AnagramTest
@@ -8,6 +9,49 @@ namespace AnagramTest
         [TestMethod]
         public void Anagram1()
         {
+            var result = Program.Anagram1("hello", "llohe");
+            var result2 = Program.Anagram1("Whoa! Hi!", "Hi! Whoa!");
+            var result3 = Program.Anagram1("One One", "Two two two");
+            var result4 = Program.Anagram1("One one", "One one c");
+            var result5 = Program.Anagram1("A tree, a life, a bench", "A tree, a fence, a yard");
+
+            Assert.IsTrue(result);
+            Assert.IsTrue(result2);
+            Assert.IsFalse(result3);
+            Assert.IsFalse(result4);
+            Assert.IsFalse(result5);
+        }
+
+        [TestMethod]
+        public void Anagram2()
+        {
+            var result = Program.Anagram2("hello", "llohe");
+            var result2 = Program.Anagram2("Whoa! Hi!", "Hi! Whoa!");
+            var result3 = Program.Anagram2("One One", "Two two two");
+            var result4 = Program.Anagram2("One one", "One one c");
+            var result5 = Program.Anagram2("A tree, a life, a bench", "A tree, a fence, a yard");
+
+            Assert.IsTrue(result);
+            Assert.IsTrue(result2);
+            Assert.IsFalse(result3);
+            Assert.IsFalse(result4);
+            Assert.IsFalse(result5);
+        }
+
+        [TestMethod]
+        public void Anagram3()
+        {
+            var result = Program.Anagram3("hello", "llohe");
+            var result2 = Program.Anagram3("Whoa! Hi!", "Hi! Whoa!");
+            var result3 = Program.Anagram3("One One", "Two two two");
+            var result4 = Program.Anagram3("One one", "One one c");
+            var result5 = Program.Anagram3("A tree, a life, a bench", "A tree, a fence, a yard");
+
+            Assert.IsTrue(result);
+            Assert.IsTrue(result2);
+            Assert.IsFalse(result3);
+            Assert.IsFalse(result4);
+            Assert.IsFalse(result5);
         }
     }
 }
