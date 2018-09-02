d0d3bda230ac04dae7131a27658968202e3a5798
Daniel de Oliveira <iel_182@hotmail.com>
2018-09-02 12:47:20 -0300

    add anagram projects

---
+++ AnagramCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnagramCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AnagramTest/AnagramTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnagramTest
+{
+    [TestClass]
+    public class AnagramTest
+    {
+        [TestMethod]
+        public void Anagram1()
+        {
+        }
+    }
+}
